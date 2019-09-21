7a31f987b16af8b714d7d2ae271da1151d5b22c8
Dononono <54520381+Dononono@users.noreply.github.com>
2019-09-20 18:34:03 -0700

    Add files via upload
    The activity from the Algorithm section in Java script.

---
+++ app.js
@@ -0,0 +1,22 @@
+// Press A for a smile and press B for a frown
+input.onButtonPressed(Button.A, function () {
+
+    basic.showLeds(`
+. . . . .
+# . . . #
+. . . . .
+# . . . #
+. # # # .
+`)
+})
+
+input.onButtonPressed(Button.B, function () {
+
+    basic.showLeds(`
+. . . . .
+# . . . #
+. . . . .
+. # # # .
+# . . . #
+`)
+})
